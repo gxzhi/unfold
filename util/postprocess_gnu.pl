858f136c02409199f9765a2f37f45250f3a64ab4
gxzhi <guoxiang.zhi@gmail.com>
2017-08-13 23:22:42 +0800

    debug: convert k-point to units of 2pi/SCALE

--- util/postprocess_gnu.pl
+++ util/postprocess_gnu.pl
@@ -12,6 +12,38 @@ print "\n";
 print "OUTPUT:\n";
 print "1. bands3d.dat	# which contains kpts,en,weitht every line.\n";
 
+
+open(FILE_in, "POSCAR.uc");
+$_=<FILE_in>;
+$_=<FILE_in>;
+$usf=$_;
+for($i=0;$i<3;$i++){
+	$_=<FILE_in>;
+	@tmp=split;
+	$a[$i][0]=$tmp[0]*$usf;
+	$a[$i][1]=$tmp[1]*$usf;
+	$a[$i][2]=$tmp[2]*$usf;
+}
+$a0xa1[0]=$a[0][1]*$a[1][2]-$a[0][2]*$a[1][1];
+$a0xa1[1]=$a[0][2]*$a[1][0]-$a[0][0]*$a[1][2];
+$a0xa1[2]=$a[0][0]*$a[1][1]-$a[0][1]*$a[1][0];
+$a_vol = $a0xa1[0]*$a[2][0] + $a0xa1[1]*$a[2][1] + $a0xa1[2]*$a[2][2];
+for($i=0;$i<3;$i++){
+	$j=$i+1;
+	$j=$j%3;
+	$k=$j+1;
+	$k=$k%3;
+	$b[$i][0]=($a[$j][1]*$a[$k][2]-$a[$j][2]*$a[$k][1])/$a_vol;
+	$b[$i][1]=($a[$j][2]*$a[$k][0]-$a[$j][0]*$a[$k][2])/$a_vol;
+	$b[$i][2]=($a[$j][0]*$a[$k][1]-$a[$j][1]*$a[$k][0])/$a_vol;
+}
+print "reciprocal lattice vector:\n";
+#printf(FILE_out "%12.9f ",$k_path_length[$i]);
+printf("%12.9f %12.9f %12.9f\n",$b[0][0],$b[0][1], $b[0][2]);
+printf("%12.9f %12.9f %12.9f\n",$b[1][0],$b[1][1], $b[1][2]);
+printf("%12.9f %12.9f %12.9f\n",$b[2][0],$b[2][1], $b[2][2]);
+close(FILE_in);
+
 open(FILE_in, $unfold_inp_file);
 
 $_=<FILE_in>;
@@ -36,11 +68,19 @@ $nkpts_total = ($nkpts_path_border-1)*$nk_per_path+1;
 #warn "$nkpts_path_border $nk_per_path\n";
 
 $_=<FILE_in>;
-@kpt_path_border_old = split;
+@tmp=split;
+$kpt_path_border_old[0] = $tmp[0] * $b[0][0] + $tmp[1] * $b[1][0] + $tmp[2] * $b[2][0];
+$kpt_path_border_old[1] = $tmp[0] * $b[0][1] + $tmp[1] * $b[1][1] + $tmp[2] * $b[2][1];
+$kpt_path_border_old[2] = $tmp[0] * $b[0][2] + $tmp[1] * $b[1][2] + $tmp[2] * $b[2][2];
 $k_path_length[0]=0;
+print " k-points in units of 2pi/SCALE:\n";
 for($i=0;$i<$nkpts_path_border-1;$i++){
+	printf("%12.9f %12.9f %12.9f\n",$kpt_path_border_old[0],$kpt_path_border_old[1],$kpt_path_border_old[2]);
 	$_=<FILE_in>;
-	@kpt_path_border_new = split;
+	@tmp=split;
+	$kpt_path_border_new[0] = $tmp[0] * $b[0][0] + $tmp[1] * $b[1][0] + $tmp[2] * $b[2][0];
+	$kpt_path_border_new[1] = $tmp[0] * $b[0][1] + $tmp[1] * $b[1][1] + $tmp[2] * $b[2][1];
+	$kpt_path_border_new[2] = $tmp[0] * $b[0][2] + $tmp[1] * $b[1][2] + $tmp[2] * $b[2][2];
 	#increment of every two kpts in k path.
 	for($xi=0;$xi<3;$xi++){
 		$increment[$xi] = ( $kpt_path_border_new[$xi] - $kpt_path_border_old[$xi] ) / $nk_per_path;
